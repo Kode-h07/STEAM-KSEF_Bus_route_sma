6a55b75db32074ba9d862e3fa991b799e6027dad
Oystersaregoated <jakeko0912@gmail.com>
2024-09-21 21:57:48 +0900

    9/21

--- data_pp.py
+++ data_pp.py
@@ -1,25 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# CSV 파일 경로
 file_path = "버스정류소현황.csv"
 
-# CSV 파일 읽기
 df = pd.read_csv(file_path, encoding="euc-kr")
 
-# '위치' 컬럼에서 '경기도 가평군 설악면'이 포함된 행 필터링
 filtered_df = df[df["위치"].str.contains("경기도 가평군 설악면", na=False)].copy()
 
-# Extract latitude and longitude using .loc[]
 filtered_df.loc[:, "lat"] = filtered_df["WGS84위도"]
 filtered_df.loc[:, "lon"] = filtered_df["WGS84경도"]
 filtered_df.loc[:, "value"] = 4
 
-# Get minimum latitude and longitude
 min_lon = filtered_df["lon"].min()
 min_lat = filtered_df["lat"].min()
 
-# Scale lon and lat values
 filtered_df.loc[:, "x"] = filtered_df["lon"].apply(
     lambda x: int((x - min_lon) * (10**3))
 )
@@ -27,33 +21,18 @@ filtered_df.loc[:, "y"] = filtered_df["lat"].apply(
     lambda y: int((y - min_lat) * (10**3))
 )
 
-# Adjust x and y values to ensure they start from a base of 10
 min_x = filtered_df["x"].min()
 min_y = filtered_df["y"].min()
 filtered_df.loc[:, "x"] = filtered_df["x"].apply(lambda x: int(x - min_x) + 10)
 filtered_df.loc[:, "y"] = filtered_df["y"].apply(lambda y: int(y - min_y) + 10)
 
-# Create nodes as tuples
 filtered_df.loc[:, "nodes"] = filtered_df[["x", "y"]].apply(tuple, axis=1)
 
-# Remove duplicates based on the nodes
 filtered_df = filtered_df.drop_duplicates(subset=["nodes"])
 
 filtered_df = filtered_df.drop(columns=["시군명", "중앙차로여부", "관할관청", "위치"])
 
-# Save the result to a new CSV file
-filtered_df.to_csv("seorak.csv", encoding="euc-kr", index=False)
 
+filtered_df = filtered_df.reset_index(drop=True)
 
-plt.figure(figsize=(10, 8))
-plt.scatter(filtered_df["x"], filtered_df["y"], c="blue", marker="o")
-
-# Adding labels for each node
-
-plt.title("Bus Stop Nodes")
-plt.xlabel("X Coordinate")
-plt.ylabel("Y Coordinate")
-plt.grid()
-plt.xlim(0, filtered_df["x"].max() + 10)  # Adjust x limits for better visibility
-plt.ylim(0, filtered_df["y"].max() + 10)  # Adjust y limits for better visibility
-plt.show()
+filtered_df.to_csv("seorak.csv", encoding="euc-kr", index=True)
---
+++ index.py
@@ -0,0 +1,31 @@
+from data_pp import filtered_df
+from slime.dish import Dish
+import matplotlib.pyplot as plt
+import networkx as nx
+from matplotlib.animation import FuncAnimation, PillowWriter
+
+stations = filtered_df
+
+start_loc = (stations.at[20, "x"], stations.at[20, "y"])
+dish = Dish(
+    dish_shape=(max(stations.x) + 10, max(stations.y) + 10),
+    foods=stations,
+    start_loc=start_loc,
+    mould_shape=(5, 5),
+    init_mould_coverage=1,
+    decay=0.2,
+)
+
+dish.animate(frames=50, interval=100, filename="50steps.gif")
+plt.show()
+
+fig = plt.figure(figsize=(6.3, 5))
+nx.draw(
+    dish.food_graph,
+    dish.food_positions,
+    node_color="C1",
+    node_shape="s",
+    node_size=12,
+    with_labels=False,
+)
+plt.savefig("slime_graph_50steps.png")
